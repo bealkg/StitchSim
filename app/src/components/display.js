08d7c226814f90d9e09462f0f02bb9587e5a192c
bealkg <bealkg@appstate.edu>
2023-02-28 10:47:05 -0500

    still working on hosting the python on the page

--- app/src/components/display.js
+++ app/src/components/display.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-// import Turtle from "react-turtle";
-// import Turt from "./turt.py"
 
 class Display extends Component {
     constructor() {
@@ -9,39 +7,10 @@ class Display extends Component {
         name: "React"
         };
     } 
+    
     render() {
         return (
             <div class="turtle" onChange={this.onChangeValue}>
-                {/* <Turtle
-                    animated={true}
-                    pixelated={true}
-                    draw={turtle => {
-                        turtle.setcolor('#588157').setlinewidth(2)
-                        return function walk() {
-                        turtle
-                        .clear()
-                        .penup()
-                        .goto(0,-150)
-                        .pendown()
-                        .circle(150)
-                        .dot()
-                        .penup()
-                        .stroke()
-                    };
-                    }}
-                /> */}
-                <iframe 
-                    title="screen"
-                    src="https://trinket.io/embed/python/8f51c9b84a?outputOnly=true&runOption=run&start=result" 
-                    width="60%" 
-                    height="100%" 
-                    frameborder="0" 
-                    marginwidth="0" 
-                    marginheight="0">
-                </iframe>
-
-                {/* <iframe src="https://trinket.io/embed/python/8f51c9b84a?outputOnly=true&runOption=run&start=result" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe> */}
-
             </div>
         );
     }
--- app/src/components/turt.py
+++ app/src/components/turt.py
@@ -1,21 +1,24 @@
 import turtle
 import math
 
+points = []
+connections = []
+
 t = turtle.Turtle()
 
 # turtle settings
 t.shape("circle")
 t.shapesize(0.2, 0.2, 0.2)
-t.speed(50)
+# t.speed(0)
+t._tracer(False)
 
 # draw frame
-# t.penup()
-# t.goto(200,200)
-# t.pendown()
-# t.goto(200,-200)
-# t.goto(-200,-200)
-# t.goto(-200,200)
-# t.goto(200,200)
+t.penup()
+t.goto(-200,-200)
+t.pendown()
+for f in range(4):
+    t.fd(400)
+    t.lt(90)
 # t.penup()
 
 # function to draw points along circle perimeter
@@ -23,41 +26,61 @@ t.speed(50)
 def drawCircle(x):
     # draw circle
     t.penup()
-    t.goto(0, -200)
+    t.goto(0, -180)
     t.pendown()
-    t.color("blue")
-    t.circle(200)
+    t.circle(180)
     t.penup()
     
     # add points
-    t.color("red")
-    t.goto(int(math.ceil(720/x)),-200)
+    t.goto(int(math.ceil(720/x)),-180)
+    
+    t.hideturtle()
     for i in range(x):
-        t.stamp()
-        t.lt(360/x)
-        t.fd((400*math.pi)/x)
+        t.goto(0,0)
+        t.fd(180)
+        t.dot()
+        points.append(t.pos())
+        if (t.heading() >= 0 and t.heading() <= 45) or (t.heading() >= 135 and t.heading() <= 225):
+            t.fd(10)
+        elif (t.heading() > 45 and t.heading() < 135):
+            t.fd(5)
+        elif (t.heading() >= 225 and t.heading() <= 360):
+            t.fd(15)
+        t.color("green")
+        t.write(i, False, align="center", font=("Arial", 10, "normal"))
+        t.color("black")
+        t.left(360/x)
 
-# drawCircle(72)
+def connectPoints(x, y):
+    t.goto(0,0)
+    t._tracer(True)
+    t.speed(20)
+    i = 0
+    while i < x:
+        t.goto(points[i])
+        t.pendown()
+        j = (i*y) % x
+        t.goto(points[j])
+        connections.append((i,j))
+        t.penup()
+        i+=1
 
 def drawSquare(x):
-    t.goto(-200,-200)
+    t.penup()
+    t.goto(-180,-180)
     t.pendown()
     for f in range(4):
-        if x % 4 != 0:
-            y = x + 1
-        for i in range(math.ceil(int(x/4))):
-            t.stamp()
-            t.fd(1600/x)
+        for i in range(math.floor(int(x/4))):
+            t.dot()
+            points.append(t.pos())
+            t.color("blue")
+            t.write(i, False, align="center", font=("Arial", 10, "normal"))
+            t.color("black")
+            t.fd(360/(math.floor(int(x/4))))
         t.lt(90)
-        
-# t.color("red")        
-drawSquare(50)
-# t.color("blue")
-# drawSquare(25)
-# t.color("green")
-# drawSquare(20)
-# t.color("yellow")
-# drawSquare(12)
-# t.color("black")
-# drawSquare(10)
+
+drawCircle(72)
+# drawSquare(72)
+connectPoints(72, 2)
+
 turtle.mainloop()
